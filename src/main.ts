1a18ea3afc4092d55d899e573d072386c72f61a0
Dylan Primera <dylanprimera98@gmail.com>
2026-01-08 16:31:07 -0500

    git ignore docker compose update readme global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true, // to ransform DTOS
+      transformOptions: {
+        exposeUnsetFields: false,
+        enableImplicitConversion: true, // help to know the type of the DTO
+      },
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
