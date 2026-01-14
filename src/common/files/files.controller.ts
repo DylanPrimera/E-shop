a87a54119276a289962127bfe0ded3effccf0500
Dylan Primera <dylanprimera98@gmail.com>
2026-01-14 12:12:30 -0500

    file filter to validate file extension

--- src/common/files/files.controller.ts
+++ src/common/files/files.controller.ts
@@ -4,17 +4,26 @@ import {
   Body,
   UploadedFile,
   UseInterceptors,
+  BadRequestException,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+    }),
+  )
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
+    if (!file)
+      throw new BadRequestException('Make sure that the file is an image');
+
     return file.originalname;
   }
 }
