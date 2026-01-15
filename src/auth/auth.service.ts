46d3b1f94c383685de090215fbdb493a85a18599
Dylan Primera <dylanprimera98@gmail.com>
2026-01-15 14:58:11 -0500

    user login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
   BadRequestException,
   Injectable,
   InternalServerErrorException,
   Logger,
+  UnauthorizedException,
 } from '@nestjs/common';
-import { CreateUserDto } from './dto';
+import { CreateUserDto, LoginDto } from './dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
@@ -26,14 +30,34 @@ export class AuthService {
         ...userData,
         password: bcrypt.hashSync(password, 10),
       });
+      const { password: _, ...userWithoutPassword } = user as any;
       await this.userRepository.save(user);
-      delete user?.password;
 
       // TODO return JWT
-      return user;
+      return userWithoutPassword;
     } catch (error) {
       this.handleExceptions(error);
     }
+  }
+
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: {
+        email: true,
+        password: true,
+      },
+    });
+
+    if (!user) throw new UnauthorizedException('Invalid credentials');
+
+    if (!bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException('Invalid credentials');
+
+    // TODO return JWT
+    return user;
   }
 
   private handleExceptions(error: any): never {
