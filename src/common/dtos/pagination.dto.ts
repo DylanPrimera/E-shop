f58a72c1c2514084706fae25078064bcbcc0a51e
Dylan Primera <dylanprimera98@gmail.com>
2026-08-31 13:21:27 -0500

    Pagination DTO docs

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,13 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsPositive, Min } from 'class-validator';
 
 export class PaginationDto {
+  @ApiProperty({
+    example: 10,
+    description: 'Max number of items to return',
+  })
   @IsOptional()
   @IsPositive()
   // @Type(() => Number) para transformar el valor desde el DTO
   limit?: number;
 
+  @ApiProperty({
+    example: 0,
+    description: 'Number of items to skip (min 0)',
+  })
   @IsOptional()
-  @IsPositive()
   @Min(0)
   offset?: number;
 }
