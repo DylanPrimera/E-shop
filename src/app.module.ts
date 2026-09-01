77a3cd274c4293c5c5eba3291dc2a2da8bb1bcf5
Dylan Primera <dylanprimera98@gmail.com>
2026-09-01 17:44:18 -0500

    setting ssl for managing neon DB instance

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,6 +18,7 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT!,
